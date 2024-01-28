9b486f8caf9cae6754e322a26d588d618237bba1
Miles <milesleejordan@gmail.com>
2024-01-27 21:30:58 -0600

    Second Commit

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'L0wfat'
     SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
-                               "mysql+mysqldb://root:admin@db:3306/Activities")
+                               "mysql+mysqldb://root:admin@127.0.0.1:3306/Activities")
